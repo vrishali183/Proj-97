06c22f9bbba1911084154242fdf8033e395e80c6
vrishali183 <vrishali183@gmail.com>
2021-12-01 21:31:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+var canvas = new fabric.Canvas('MyCanvas');
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image, function(Img){
+		block_image_object = Img;
+
+		block_image_object.scaleToWidth(block_image_width);
+		block_image_object.scaleToHeight(block_image_height)
+		block_image_object.set({
+		top:block_y,
+		left:block_x
+		});
+		canvas.add(block_image_object);
+	});
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') 
+	{
+		new_image('Ron.png')
+		console.log("r")
+	}
+	if(keyPressed == '71')
+	{
+		block_x =200 ;
+		new_image('Hermoine.png')
+		console.log("g")
+	}
+	
+	if(keyPressed == '72')
+	{
+		block_x =350;
+		new_image('Harry_Potter.png')
+		console.log("h")
+	}
+	
+	if(keyPressed == '68')
+	{
+		block_x =700 ;
+	    new_image('Dumbledore.jpg')
+		console.log("d")
+	}
+	
+}
+
